dfba4a33a427bc9e77d7a143a94cbf866b176b6a
Olivier-Baptiste Hansen <oliha@itu.dk>
2025-02-17 19:20:04 +0100

    Add simple heuristic checks first
    Co-authored-by: Niklas <nizc@itu.dk>

--- minimax/minimax.py
+++ minimax/minimax.py
@@ -14,7 +14,10 @@ def max_value(state: chess.Board, move: chess.Move, depth: int, alpha: int, beta
 
     best_move: Result = Result(None, float('-inf'))
 
-    for a in state.legal_moves:
+    legal_moves = list(state.legal_moves)
+    legal_moves.sort(key=lambda a: state.gives_check(a))
+
+    for a in legal_moves:
         state2 = state.copy()
         state2.push(a)
         result: Result = min_value(state2, a, depth + 1, alpha, beta)
@@ -33,7 +36,10 @@ def min_value(state: chess.Board, move: chess.Move, depth: int, alpha: int, beta
     
     best_move: Result = Result(None, float('inf'))
 
-    for a in state.legal_moves:
+    legal_moves = list(state.legal_moves)
+    legal_moves.sort(key=lambda a: state.gives_check(a))
+
+    for a in legal_moves:
         state2 = state.copy()
         state2.push(a)
         result: Result = max_value(state2, a, depth + 1, alpha, beta)
@@ -61,12 +67,15 @@ m5 = chess.Board("1k6/8/2Q5/8/8/2K5/8/8 w - - 0 1")
 m3 = chess.Board("1k6/8/2QK4/8/8/8/8/8 w - - 0 1")
 rm3 = chess.Board("8/2k5/5R2/4R3/8/8/8/3K4 w - - 0 1")
 rm2 = chess.Board("8/k7/6RR/8/8/8/8/4K3 w - - 0 1")
+m3_loaded = chess.Board("b5k1/1q3p1p/3p2p1/6Q1/1p3N2/1Prn3P/2P3P1/5RK1 w - - 0 27")
 
 # Current lazy way of printing mate sequence (Test for an existing sequence before calling if unsure)
 def print_sequence(state: chess.Board):
     while (state.is_game_over() != True):
         move = search(state)
-        print(move)
+        print(state.san(move))
         state.push(move)
+        global CUTOFF
+        CUTOFF -= 1
 
-print_sequence(rm2)
+print_sequence(m3_loaded)
