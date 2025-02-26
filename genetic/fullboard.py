ae727158ec22d72f31060f0be3af460933b9bfa9
Olivier-Baptiste Hansen <oliha@itu.dk>
2025-02-26 18:01:25 +0100

    Create simple implementation of GA

--- genetic/fullboard.py
+++ genetic/fullboard.py
@@ -1,21 +1,36 @@
 from .genetic import Genetic
 from common.evaluator import Evaluator, EvaluatorResponse
-from .utility import chess_board_to_int
+from .utility import chess_board_to_int, chess_int_to_board
 from random import randint
 import chess
 
+
 class FullBoard(Genetic):
-    def __init__(self, evaluator: Evaluator=None):
+    def __init__(self, evaluator: Evaluator = None):
         super().__init__(evaluator)
 
-    
     def _fitness(self, chromosome: list[int]) -> float:
-        raise NotImplementedError()
-    
+        board = chess_int_to_board(chromosome)
+        if not board.is_valid():
+            return -10
+        evaluation: EvaluatorResponse = self.evaluator.run(board)
+        if evaluation.has_mate:
+            print("Mate found!")
+            return 10
+        else:
+            return 0
 
     def _mutate(self, population: list[list[int]]) -> list[list[int]]:
-        raise NotImplementedError()
+        for i in range(len(population)-1):
+            if randint(0, 100) < 20:
+                board = chess_int_to_board(population[i])
+                for _ in range(randint(1, 10)):
+                    legal_moves = board.legal_moves
+                    board.push(list(legal_moves)[randint(0, legal_moves.count() - 1)])
+                board = chess_board_to_int(board)
+                population[i] = board
 
+        return population
 
     # Create population
     def _create_population(self, amount: int) -> list[list[int]]:
@@ -29,25 +44,33 @@ class FullBoard(Genetic):
                 if legal_moves.count() == 0:
                     break
                 board.push(list(legal_moves)[randint(0, legal_moves.count() - 1)])
-            
+
             board_int = chess_board_to_int(board)
-            
+
             population.append(board_int)
-        
+
         return population
-    
 
     def _run_tournament(self, population: list[list[int]], evaluations: list[float]) -> list[list[int]]:
-        raise NotImplementedError()
-        """ selection = list(zip(population, evaluations))
+        selection = list(zip(population, evaluations))
         selection.sort(key=lambda x: x[1])
-        selection = selection[len(population)/2:]
-        return selection """
-    
+        selection = selection[len(population)//2:]
+        selection = list(map(lambda x: x[0], selection))
+        return selection
+
+    def _mate(self, parent1: list[int], parent2: list[int]) -> list[list[int]]:
+        half = len(parent1) // 2
+        return [parent1[:half] + parent2[half:], parent2[:half] + parent1[half:]]
 
     def _reproduce(self, population: list[list[int]]) -> list[list[int]]:
-        raise NotImplementedError()
-    
-    
+        offspring = []
+        for i in range(0, len(population)-1, 2):
+            children = self._mate(population[i], population[i+1])
+            offspring += children
+        if len(population) % 2 != 0:
+            children = self._mate(population[0], population[-1])
+            offspring.append(children[0])
+        return offspring
+
     def _stop_condition(self, generation) -> bool:
-        raise NotImplementedError()
+        return False
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from chess_engine import ChessEngine
+from genetic.utility import chess_int_to_board
 from genetic import FullBoard
-from genetic.utility import chess_int_to_board, chess_board_to_int
 import chess
 
 # Test
 engine = ChessEngine(5)
-test_board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-res = engine.run(test_board)
 
-#genetic = FullBoard(engine)
-#genetic.run(5, 10)
+genetic = FullBoard(engine)
+population, evaluations = genetic.run(20, 20)
+
+for c in population:
+    board = chess_int_to_board(c)
+    print(board.fen())
