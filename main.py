f929b9491a74660dff8eecc1a0f402789315d877
Niklas <nizc@itu.dk>
2025-04-11 14:03:15 +0200

    add tournament in fullboard, and create two-point crossover

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from multiprocessing import freeze_support
 from common.conditions import Conditions
 from genetic import Composer
+from genetic import FullBoard
 from genetic.utility import chess_int_to_board
 
 if __name__ == '__main__':
     freeze_support()
     genetic = Composer(5)
     # Conditions of the form: Time, Generation, Evaluation limit
-    conditions = Conditions(None,50,10)
-    result = genetic.run(30, conditions)
+    conditions = Conditions(None,50,None)
+    result = genetic.run(20, conditions)
 
     for c in result:
         board = chess_int_to_board(c.body)
