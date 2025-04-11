adf81e7cceb94d10ebe91c217e50dd13509cb786
zntheory <racheeeve@gmail.com>
2025-04-11 05:38:24 +0200

    feat: Create print-method to print all fields for EvaluatorResponse

--- common/evaluator.py
+++ common/evaluator.py
@@ -1,15 +1,23 @@
 import chess
 
+
 class Line:
     def __init__(self, line: list[chess.Move]):
         self.line = line
 
+
 class EvaluatorResponse:
-    def __init__(self, fen: str, has_mate: bool, utility: float, moves):
+    def __init__(self, fen: str, has_mate: bool, utility: float, moves: Line):
         self.fen = fen
         self.has_mate = has_mate
         self.utility = utility
         self.moves = moves
+
+    def print(self):
+        print(self.fen)
+        print(self.has_mate)
+        print(self.utility)
+        print(self.moves.line)
 
 
 class Evaluator:
