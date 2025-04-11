221dbfbebb95c37123c6243f635e9a7c0c4dba44
zntheory <racheeeve@gmail.com>
2025-04-11 05:45:06 +0200

    feat: Create Handmaiden(Fitness) ... to find boards where either the (white) Queen checkmates, or the (black) king will get checked by 2 or more unique piece-types.

---
+++ genetic/fitness/handmaiden.py
@@ -0,0 +1,24 @@
+from common.evaluator import EvaluatorResponse
+from feature_detector.feature_detector import FeatureDetector
+from .base import Fitness
+
+
+class Handmaiden(Fitness):
+    def __init__(self):
+        super().__init__()
+        self.fd = FeatureDetector()
+
+    def _evaluate_fitness(self, evaluation: EvaluatorResponse) -> float:
+        res = 0
+        if not evaluation.has_mate:
+            return res
+
+        feats = self.fd.run(evaluation)
+
+        if evaluation.utility > 0:              res += 1
+        if len(evaluation.moves.line) > 1:      res += 1
+        if len(feats.kingkillers) > 1:          res += 10
+        if feats.kingkillers.__contains__(5):   res += 8; print("- Kingkiller Queen!")
+        if len(feats.checkers) > 1: res += 10; print("- Multi-checkers!");
+        return res
+
