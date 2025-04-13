91cfcc71ede612a2d70f30d02652c752775243c1
Olivier-Baptiste Hansen <oliha@itu.dk>
2025-04-13 21:09:22 +0200

    Add making random moves

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from multiprocessing import freeze_support
 from common.conditions import Conditions
 from genetic import Composer
-from genetic import FullBoard
 from genetic.utility import chess_int_to_board
 
 if __name__ == '__main__':
     freeze_support()
     genetic = Composer(5)
     # Conditions of the form: Time, Generation, Evaluation limit
-    conditions = Conditions(None,50,None)
+    conditions = Conditions(None, 50, None)
     result = genetic.run(20, conditions)
 
     for c in result:
