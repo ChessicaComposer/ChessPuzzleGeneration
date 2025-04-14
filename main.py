cd7992bd7ef51180b6089f35fbb5a6b23be391da
zntheory <racheeeve@gmail.com>
2025-04-14 16:16:19 +0200

    feat: Add ply as arg. Minor cosmetic changes.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import time
 from multiprocessing import freeze_support
 from common.conditions import Conditions
 from genetic import Composer
@@ -10,7 +10,7 @@ from genetic.utility import chess_int_to_board
 
 class Main(cmd.Cmd):
     prompt = 'ChessPuzzleGeneration>> '
-    intro = 'Welcome to the ChessPuzzleGenerator! Type "help" or "?" to list commands.'
+    intro = 'Welcome to the ChessPuzzleGenerator! Type "help" or "?" to list commands.\n Usage: run <Composer | FullBoard> <Ply: int> <Population: int> <Time-limit: int | None> <Generation-limit: int | None> <Evaluation-limit: int | None>'
 
     def __init__(self):
         super().__init__()
@@ -31,46 +31,56 @@ class Main(cmd.Cmd):
             return
 
         result = list()
+        time_ = 0.0
 
         match args[0]:
             case "help":
                 if len(args) > 1:
-                    self.__run_help__(args[1])
+                    self._run_help(args[1])
                     return
             case "composer":
-                conditions = self.__format_conditions__(args)
+                conditions = self._format_conditions(args)
                 if conditions is None: return
-                result = Composer().run(int(args[1]), conditions)
+                start_time = time.time()
+                result = Composer(int(args[1])).run(int(args[2]), conditions)
+                time_ = time.time() - start_time
             case "fullboard":
-                conditions = self.__format_conditions__(args)
+                conditions = self._format_conditions(args)
                 if conditions is None: return
-                result = FullBoard().run(int(args[1]), conditions)
+                start_time = time.time()
+                result = FullBoard(int(args[1])).run(int(args[2]), conditions)
+                time_ = time.time() - start_time
             case _:
                 print("Unknown generator type.")
                 return
 
-        if len(args) < 5:
-            self.__print_insufficient_args__()
+        if len(args) < 6:
+            self._print_insufficient_args()
             return
 
         for c in result:
             board = chess_int_to_board(c.body)
             print(board.fen())
+
+        
+        self._print_args(args)
+        print("Elapsed generation time: ", time_, " seconds")
+
         return
 
-    def __format_conditions__(self, args: list[str]) -> Conditions | None:
+    def _format_conditions(self, args: list[str]) -> Conditions | None:
         if len(args) < 5:
-            self.__print_insufficient_args__()
+            self._print_insufficient_args()
             print("- Example: run composer 100 None 100 None")
             return None
         aux = []
-        for arg in args[2:]:
+        for arg in args[3:]:
             if arg == 'none':
                 aux.append(None)
             else: aux.append(float(arg))
         return Conditions(aux[0], aux[1], aux[2])
 
-    def __run_help__(self, arg):
+    def _run_help(self, arg):
         match arg:
             case "generator-type" | "generator" | "composer" | "fullboard":
                 print("Available generator types:\n"
@@ -78,24 +88,43 @@ class Main(cmd.Cmd):
                       "FullBoard | Composer\n"
                       "- FullBoard: Initialises a standard chess board. Randomly moves pieces.\n"
                       "- Composer: Initialises a chess board with each king in some random, valid position. Randomly adds pieces.\n")
+            case "ply-size" | "ply":
+                print("Ply size: How many moves ahead the generator will evaluate the board's state.")
             case "population-size" | "population":
                 print("Population size: How many boards to initialise.\n"
                       "- A generation will always keep this population size.")
             case "time-limit" | "time":
-                print("Time limit: How long the generator will run in seconds.")
+                print("Time limit: How long the generator will run in seconds.\n"
+                      "- If time's exceeded, it will finish its current generation before exiting.")
             case "generation-limit" | "generation":
                 print("Generation limit: How many changes a board will undergo.")
             case "evaluation-limit" | "evaluation":
                 print("Evaluation limit: The average score a generation of boards may get.")
             case _:
-                print("Unknown argument type. Try 'run help'.")
+                print("Unknown argument type. Try 'run help', ex: 'run help generator'.")
         return
 
-    def __print_insufficient_args__(self):
+    def _print_insufficient_args(self):
         print("Insufficient number of arguments.\n"
               "===================================\n"
-              "- Usage: run <generator-type> <population-size> <time-limit or None> <generation-limit or None> <evaluation-limit or None>\n"
+              "- Usage: run <generator-type> <ply-size> <population-size> <time-limit or None> <generation-limit or None> <evaluation-limit or None>\n"
               "- More info: run help <type>")
+
+    def _print_args(self, args):
+        generator = args[0]
+        ply = args[1]
+        population = args[2]
+        time_limit = args[3]
+        generation_limit = args[4]
+        evaluation_limit = args[5]
+        print("\n==========================\n",
+              "Generator: ", generator,
+              " | Ply: ", ply,
+              " | Population: ", population,
+              " | Time-limit: ", time_limit,
+              " | Generation-limit: ", generation_limit,
+              " | Evaluation-limit: ", evaluation_limit)
+
 
     def postloop(self):
         print("Have a nice day!")
